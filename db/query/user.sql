66705fbb32a28bfb00fb177d321133da4334d531
XuanHieu <hoxuanhieu.pch.2b1.92@gmail.com>
2023-09-28 14:29:13 +0700

    update

--- db/query/user.sql
+++ db/query/user.sql
@@ -12,19 +12,6 @@ INSERT INTO users (
   $1, $2, $3, $4, $5, $6, $7, $8
 ) RETURNING *;
 
--- name: CreateUserNoProvince :one
-INSERT INTO users (
-  username,
-  hashed_password,
-  full_name,
-  email,
-  phone,
-  address,
-  role
-) VALUES (
-  $1, $2, $3, $4, $5, $6, $7
-) RETURNING *;
-
 -- name: GetUser :one
 SELECT * FROM users
 WHERE username = $1 LIMIT 1;
